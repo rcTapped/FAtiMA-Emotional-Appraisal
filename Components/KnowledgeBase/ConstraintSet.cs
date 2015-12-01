3f50d869bc6f88851ea95b886e3f326a332c39b9
Pedro Gonçalves <pedro0rsg@gmail.com>
2015-12-01 10:55:30 +0000

    - Added ConstraintSet class to project with some tests on it.    - At the moment, ConstraintSet is not working properly

---
+++ Components/KnowledgeBase/ConstraintSet.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KnowledgeBase.WellFormedNames;
+using Utilities;
+
+namespace KnowledgeBase
+{
+	public sealed class ConstraintSet
+	{
+		private Dictionary<Name, HashSet<Node>> m_links;
+		private HashSet<Constraint> m_constraints;
+
+		public ConstraintSet()
+		{
+			m_links=new Dictionary<Name, HashSet<Node>>();
+			m_constraints=new HashSet<Constraint>();
+		}
+
+		public bool Add(Constraint constraint)
+		{
+			if (constraint.Variable.Equals(constraint.Value))
+				return true;
+
+			if (m_constraints.Contains(constraint))
+				return true;
+
+			if (!CanAdd(constraint))
+				return false;
+
+			LinkConstraint(constraint);
+			m_constraints.Add(constraint);
+			return true;
+		}
+
+		public void Clear()
+		{
+			m_links.Clear();
+			m_constraints.Clear();
+		}
+
+		private bool CanAdd(Constraint c)
+		{
+			Name v1 = GetValue(c.Variable);
+			if (v1 == null)
+				return !FindInequality(c.Variable, c.Value.IsVariable?GetValue((Symbol)c.Value):c.Value, c.Type == Constraint.ConstraintType.Equals);
+
+			Name v2;
+			if (c.Value.IsVariable)
+			{
+				v2 = GetValue((Symbol) c.Value);
+				if (v2 == null)
+					return !FindInequality((Symbol) c.Value, v1, c.Type == Constraint.ConstraintType.Equals);
+			}
+			else
+				v2 = c.Value;
+
+			var result = v1.Equals(v2);
+			return c.Type == Constraint.ConstraintType.Equals ? result : !result;
+		}
+
+		private void LinkConstraint(Constraint c)
+		{
+			HashSet<Node> nodes;
+			if (!m_links.TryGetValue(c.Variable, out nodes))
+			{
+				nodes = new HashSet<Node>();
+				m_links[c.Variable] = nodes;
+			}
+			nodes.Add(new Node(c.Value, c.Type == Constraint.ConstraintType.Equals));
+
+			if (!m_links.TryGetValue(c.Value, out nodes))
+			{
+				nodes = new HashSet<Node>();
+				m_links[c.Value] = nodes;
+			}
+			nodes.Add(new Node(c.Variable, c.Type == Constraint.ConstraintType.Equals));
+		}
+
+		private Name GetValue(Symbol variable)
+		{
+			HashSet<Symbol> explored = new HashSet<Symbol>();
+			Queue<Symbol> next = new Queue<Symbol>();
+			next.Enqueue(variable);
+			while (next.Count>0)
+			{
+				var v = next.Dequeue();
+				explored.Add(v);
+				HashSet<Node> nodes;
+				if (m_links.TryGetValue(v, out nodes))
+				{
+					foreach (var e in nodes.Where(n => n.Equal).Select(n => n.Link))
+					{
+						if (!e.IsVariable)
+							return e;
+						if (!explored.Contains((Symbol)e))
+							next.Enqueue((Symbol)e);
+					}
+				}
+			}
+			return null;
+		}
+
+		private bool AreEqual(Symbol v1, Symbol v2)
+		{
+			if (v1.Equals(v2))
+				return true;
+
+			HashSet<Symbol> explored = new HashSet<Symbol>();
+			Queue<Symbol> next = new Queue<Symbol>();
+			next.Enqueue(v1);
+			while (next.Count > 0)
+			{
+				var v = next.Dequeue();
+				explored.Add(v);
+				HashSet<Node> nodes;
+				if (m_links.TryGetValue(v, out nodes))
+				{
+					foreach (var e in nodes.Where(n => n.Equal && n.Link.IsVariable).Select(n => (Symbol)n.Link))
+					{
+						if (e.Equals(v2))
+							return true;
+						if (!explored.Contains(e))
+							next.Enqueue(e);
+					}
+				}
+			}
+			return false;
+		}
+
+		private bool FindInequality(Symbol variable, Name value, bool equal)
+		{
+			if (value == null)
+				return false;
+
+			if (equal)
+				return FindInequality2(variable, value);
+
+			HashSet<Node> nodes;
+			if (m_links.TryGetValue(value, out nodes))
+			{
+				foreach (var l in nodes.Where(n => !n.Equal).Select(n => (Symbol)n.Link))
+				{
+					if (AreEqual(variable, l))
+						return true;
+				}
+			}
+			return false;
+		}
+
+		private bool FindInequality2(Symbol variable, Name value)
+		{
+			HashSet<Node> nodes;
+			if (m_links.TryGetValue(variable, out nodes))
+			{
+				foreach (var l in nodes.Where(n => n.Link.IsVariable))
+				{
+					if (l.Equal)
+					{
+						if(fi)
+					}
+					var v = GetValue(l);
+					if (v != null && v.Equals(value))
+						return true;
+				}
+			}
+		}
+
+		private class Node
+		{
+			public readonly Name Link;
+			public readonly bool Equal;
+
+			public Node(Name link, bool equal)
+			{
+				Link = link;
+				Equal = equal;
+			}
+
+			public override int GetHashCode()
+			{
+				var h = Link.GetHashCode();
+				return Equal ? h : ~h;
+			}
+
+			public override bool Equals(object obj)
+			{
+				var n = obj as Node;
+				if (n == null)
+					return false;
+				return Link.Equals(n.Link) && (n.Equal == Equal);
+			}
+		}
+	}
+}
---
+++ Tests.KnowledgeBase/ConstraintSetTests.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KnowledgeBase;
+using NUnit.Framework;
+using Utilities;
+
+namespace Tests.KnowledgeBase
+{
+	[TestFixture]
+	public class ConstraintSetTests
+	{
+		private class TestFactory
+		{
+			public IEnumerable<TestCaseData> Test_Add_Batch_Cases()
+			{
+				yield return new TestCaseData(new[]
+				{
+					"[x]=[x]"
+				}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]=4"
+				//}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]=4",
+				//	"[x]=3"
+				//}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]=5",
+				//	"[x]=4"
+				//}, true);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=[y]",
+				//	"[x]=3"
+				//}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[x]=[y]"
+				//}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[x]=[y]",
+				//	"[y]=4"
+				//}, true);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]=3",
+				//	"[x]=[w]",
+				//	"[z]=[y]",
+				//	"[x]=[y]"
+				//}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]=4",
+				//	"[x]=[w]",
+				//	"[z]=[y]",
+				//	"[x]=[y]"
+				//}, true);
+
+				////Inequalities
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[y]!=3",
+				//	"[x]=3",
+				//	"[y]=4"
+				//}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]!=3",
+				//	"[y]=4"
+				//}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]=4",
+				//	"[y]!=3",
+				//}, false);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]!=4",
+				//	"[y]=[x]",
+				//}, false);
+
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]=3",
+				//	"[y]!=3",
+				//}, true);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[x]=3",
+				//	"[y]=3",
+				//	"[y]!=[x]",
+				//}, true);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[y]!=[x]",
+				//	"[x]=3",
+				//	"[y]=3",
+				//}, true);
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[y]!=[x]",
+				//	"[x]=3",
+				//	"[y]=4",
+				//	"[y]=[z]",
+				//	"[x]=[z]",
+				//}, true);
+
+				yield return new TestCaseData(new[]
+				{
+					"[x]!=3",
+					"[x]=[z]",
+					"[w]=[z]",
+					"[x]=[t]",
+					"[y]=[z]",
+					"[y]=3",
+				}, true);
+
+				yield return new TestCaseData(new[]
+				{
+					"[x]=3",
+					"[x]=[z]",
+					"[w]=[z]",
+					"[x]=[t]",
+					"[a]!=[w]",
+					"[a]!=[x]",
+					"[y]!=[z]",
+					"[y]=3",
+				}, true);
+
+				yield return new TestCaseData(new[]
+				{
+					"[x]=[z]", "[a]!=[w]", "[y]!=[z]", "[w]=[z]", "[y]=3", "[x]=[t]", "[a]!=[x]", "[x]=3"
+				}, true);
+
+				////Complex
+				//yield return new TestCaseData(new[]
+				//{
+				//	"[a]=6",
+				//	"[f]=3",
+				//	"3!=[c]",
+				//	"[d]=3",
+				//	"[g]=5",
+				//	"[a]=[e]",
+				//	"[e]!=5",
+				//	"[b]!=6",
+				//	"[a]=[c]",
+				//	"[a]!=3",
+				//	"[g]!=[f]",
+				//	"[f]!=5",
+				//	"[b]!=6",
+				//	"[c]=6",
+				//	"[b]=5",
+				//}, false);
+			}
+		}
+
+		[TestCaseSource(typeof(TestFactory),"Test_Add_Batch_Cases")]
+		public void Test_Add_Batch(IEnumerable<string> constraints, bool shouldFail)
+		{
+			const int iterations = 1000;
+			Constraint[] elements = constraints.Select(s => (Constraint) s).ToArray();
+			var set = new ConstraintSet();
+			int it = iterations;
+			while (it>0)
+			{
+				bool fail = false;
+				try
+				{
+					foreach (var c in elements)
+					{
+						if (set.Add(c))
+						{
+							if (shouldFail)
+								fail = true;
+						}
+						else
+						{
+							if (shouldFail)
+								Assert.Pass();
+						}
+					}
+
+					if (fail || shouldFail)
+					{
+						Console.WriteLine("Iteration {0}",(iterations - it));
+						Console.Write(elements.Select(s => '\"' + s.ToString() + '\"').AggregateToString(", "));
+						Assert.Fail();	
+					}
+				}
+				catch (SuccessException)
+				{
+					
+				}
+	
+				set.Clear();
+				elements = elements.OrderBy(s => Guid.NewGuid()).ToArray();
+				it--;
+			}
+		}
+	}
+}
